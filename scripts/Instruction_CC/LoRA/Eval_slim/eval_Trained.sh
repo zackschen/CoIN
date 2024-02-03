2493a49d891a42144dd4b39b4d7be04cffddec7f
Zacks <cczacks@gmail.com>
2024-02-03 22:29:10 +0800

    modify test slim

--- scripts/Instruction_CC/LoRA/Eval_slim/eval_Trained.sh
+++ scripts/Instruction_CC/LoRA/Eval_slim/eval_Trained.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/1_eval_sqa.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/ScienceQA/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/TextVQA/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/ImageNet/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/4_eval_gqa.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/GQA/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/5_eval_vizwiz.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VizWiz/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/6_eval_visualgenome.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/Grounding/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/7_eval_vqav2.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/8_eval_ocrvqa.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/1_eval_sqa.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/ScienceQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/TextVQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/ImageNet/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/4_eval_gqa.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/GQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/5_eval_vizwiz.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VizWiz/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/6_eval_visualgenome.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/Grounding/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/7_eval_vqav2.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/8_eval_ocrvqa.sh Finetune ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/1_eval_sqa.sh TextVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/TextVQA/llava-1.5-7b-lora
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/1_eval_sqa.sh ImageNet ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/ImageNet/llava-1.5-7b-lora
@@ -17,29 +17,29 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Ins
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/1_eval_sqa.sh VQAv2 ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/1_eval_sqa.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh ImageNet ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/ImageNet/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh GQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/GQA/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh VizWiz ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VizWiz/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh Grounding ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/Grounding/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh VQAv2 ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh ImageNet ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/ImageNet/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh GQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/GQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh VizWiz ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VizWiz/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh Grounding ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/Grounding/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh VQAv2 ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/2_eval_textqa.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh GQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/GQA/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh VizWiz ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VizWiz/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh Grounding ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/Grounding/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh VQAv2 ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh GQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/GQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh VizWiz ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VizWiz/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh Grounding ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/Grounding/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh VQAv2 ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/3_eval_ImageNet.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/4_eval_gqa.sh VizWiz ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VizWiz/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/4_eval_gqa.sh Grounding ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/Grounding/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/4_eval_gqa.sh VQAv2 ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/4_eval_gqa.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/4_eval_gqa.sh VizWiz ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VizWiz/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/4_eval_gqa.sh Grounding ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/Grounding/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/4_eval_gqa.sh VQAv2 ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/4_eval_gqa.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/5_eval_vizwiz.sh Grounding ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/Grounding/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/5_eval_vizwiz.sh VQAv2 ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/5_eval_vizwiz.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/5_eval_vizwiz.sh Grounding ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/Grounding/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/5_eval_vizwiz.sh VQAv2 ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/5_eval_vizwiz.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/6_eval_visualgenome.sh VQAv2 ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/6_eval_visualgenome.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/6_eval_visualgenome.sh VQAv2 ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/VQAv2/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/6_eval_visualgenome.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/7_eval_vqav2.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash /home/chencheng/Code/LLaVA/scripts/Instruction_CC/LoRA/Eval_slim/7_eval_vqav2.sh OCRVQA ./checkpoints/Instruction/Only_Pretrain_1.5_Memory_100/OCRVQA/llava-1.5-7b-lora
