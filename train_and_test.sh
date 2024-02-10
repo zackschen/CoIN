b9a051b8f486855dea50fdae693a82b53194e975
Zacks <cczacks@gmail.com>
2024-02-10 22:57:02 +0800

    modify train memory slim

--- train_and_test.sh
+++ train_and_test.sh
@@ -1,5 +1,5 @@
 
-bash scripts/Instruction_CC/LoRA/Train_memory_500/Train_all.sh
-bash scripts/Instruction_CC/LoRA/Eval_memory/eval_Trained.sh
+# bash scripts/Instruction_CC/LoRA/Train_memory_500/Train_all.sh
+# bash scripts/Instruction_CC/LoRA/Eval_memory/eval_Trained.sh
 bash scripts/Instruction_CC/LoRA/Train/finetune_CC_Instruction_Multitask.sh
-bash scripts/Instruction_CC/LoRA/Eval/eval_Multitask.sh
+# bash scripts/Instruction_CC/LoRA/Eval/eval_Multitask.sh
