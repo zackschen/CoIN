7355b10ce06a8f95361de85f19e68710bdc191df
Zacks <cczacks@gmail.com>
2024-03-22 09:16:01 +0800

    Adding Qwen and completing the training process

--- scripts/Qwen/1_scienceqa.sh
+++ scripts/Qwen/1_scienceqa.sh
@@ -8,7 +8,7 @@ NODE_RANK=0
 MASTER_ADDR=localhost
 MASTER_PORT=6001
 
-MODEL="./checkpoints/Qwen-VL" # Set the path if you do not want to load from huggingface directly
+MODEL="./checkpoints/Qwen-VL-Chat" # Set the path if you do not want to load from huggingface directly
 # ATTENTION: specify the path to your training data, which should be a json file consisting of a list of conversations.
 # See the section for finetuning in README for more information.
 OUTPUT_MODEL_PATH="./checkpoints/CLIT_Qwen_Finetune/ScienceQA"
