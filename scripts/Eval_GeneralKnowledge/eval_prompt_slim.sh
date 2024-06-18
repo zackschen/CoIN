fa896c9201b495f7fdad80ded5435e89c576a370
Zacks <cczacks@gmail.com>
2024-06-18 21:23:42 +0800

    merge result

--- scripts/Eval_GeneralKnowledge/eval_prompt_slim.sh
+++ scripts/Eval_GeneralKnowledge/eval_prompt_slim.sh
@@ -1,357 +1,121 @@
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/Finetune \
+    ./results/CoIN/LLaVA-10Typev2/ScienceQA/Finetune \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/GQA \
+    ./results/CoIN/LLaVA-10Typev2/ScienceQA/GQA \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/Grounding \
+    ./results/CoIN/LLaVA-10Typev2/ScienceQA/Grounding \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/ImageNet \
+    ./results/CoIN/LLaVA-10Typev2/ScienceQA/ImageNet \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/OCRVQA \
+    ./results/CoIN/LLaVA-10Typev2/ScienceQA/OCRVQA \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/TextVQA \
+    ./results/CoIN/LLaVA-10Typev2/ScienceQA/TextVQA \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/VQAv2 \
+    ./results/CoIN/LLaVA-10Typev2/ScienceQA/VQAv2 \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/VizWiz \
-
-###############################################################################################
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/Finetune \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/GQA \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/Grounding \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/ImageNet \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/OCRVQA \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/VizWiz \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/VQAv2 \
+    ./results/CoIN/LLaVA-10Typev2/ScienceQA/VizWiz \
 
 ###############################################################################################
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/Finetune \
+    ./results/CoIN/LLaVA-10Typev2/TextVQA/Finetune \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/GQA \
+    ./results/CoIN/LLaVA-10Typev2/TextVQA/GQA \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/Grounding \
+    ./results/CoIN/LLaVA-10Typev2/TextVQA/Grounding \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/OCRVQA \
+    ./results/CoIN/LLaVA-10Typev2/TextVQA/ImageNet \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/VizWiz \
+    ./results/CoIN/LLaVA-10Typev2/TextVQA/OCRVQA \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/VQAv2 \
-
-################################################################################################
+    ./results/CoIN/LLaVA-10Typev2/TextVQA/VizWiz \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/Finetune \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/Grounding \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/OCRVQA \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/VizWiz \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/VQAv2 \
-
-################################################################################################
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/Finetune \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/Grounding \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/OCRVQA \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/VQAv2 \
-
-################################################################################################
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/Grounding/Finetune \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/Grounding/OCRVQA \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/Grounding/VQAv2 \
-
-################################################################################################
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VQAv2/Finetune \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VQAv2/OCRVQA \
-
-################################################################################################
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/OCRVQA/Finetune \
-
-################################################################################################
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/Zero_Shot \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/Zero_Shot \
-
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/Zero_Shot \
-
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/Zero_Shot \
-
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/Zero_Shot \
-
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/Grounding/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/Grounding/Zero_Shot \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VQAv2/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VQAv2/Zero_Shot \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/OCRVQA/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/OCRVQA/Zero_Shot \
-
-
-################################################################################################
-################################################################################################
-################################################################################################
-
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/Finetune \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/GQA \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/Grounding \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/ImageNet \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/OCRVQA \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/TextVQA \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/VQAv2 \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/VizWiz \
+    ./results/CoIN/LLaVA-10Typev2/TextVQA/VQAv2 \
 
 ###############################################################################################
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/Finetune \
+    ./results/CoIN/LLaVA-10Typev2/ImageNet/Finetune \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/GQA \
+    ./results/CoIN/LLaVA-10Typev2/ImageNet/GQA \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/Grounding \
+    ./results/CoIN/LLaVA-10Typev2/ImageNet/Grounding \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/ImageNet \
+    ./results/CoIN/LLaVA-10Typev2/ImageNet/OCRVQA \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/OCRVQA \
+    ./results/CoIN/LLaVA-10Typev2/ImageNet/VizWiz \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/VizWiz \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/VQAv2 \
-
-###############################################################################################
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/Finetune \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/GQA \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/Grounding \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/OCRVQA \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/VizWiz \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/VQAv2 \
+    ./results/CoIN/LLaVA-10Typev2/ImageNet/VQAv2 \
 
 ################################################################################################
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/Finetune \
+    ./results/CoIN/LLaVA-10Typev2/GQA/Finetune \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/Grounding \
+    ./results/CoIN/LLaVA-10Typev2/GQA/Grounding \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/OCRVQA \
+    ./results/CoIN/LLaVA-10Typev2/GQA/OCRVQA \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/VizWiz \
+    ./results/CoIN/LLaVA-10Typev2/GQA/VizWiz \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/VQAv2 \
+    ./results/CoIN/LLaVA-10Typev2/GQA/VQAv2 \
 
 ################################################################################################
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/Finetune \
+    ./results/CoIN/LLaVA-10Typev2/VizWiz/Finetune \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/Grounding \
+    ./results/CoIN/LLaVA-10Typev2/VizWiz/Grounding \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/OCRVQA \
+    ./results/CoIN/LLaVA-10Typev2/VizWiz/OCRVQA \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/VQAv2 \
+    ./results/CoIN/LLaVA-10Typev2/VizWiz/VQAv2 \
 
 ################################################################################################
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/Grounding/Finetune \
+    ./results/CoIN/LLaVA-10Typev2/Grounding/Finetune \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/Grounding/OCRVQA \
+    ./results/CoIN/LLaVA-10Typev2/Grounding/OCRVQA \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/Grounding/VQAv2 \
+    ./results/CoIN/LLaVA-10Typev2/Grounding/VQAv2 \
 
 ################################################################################################
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VQAv2/Finetune \
+    ./results/CoIN/LLaVA-10Typev2/VQAv2/Finetune \
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VQAv2/OCRVQA \
+    ./results/CoIN/LLaVA-10Typev2/VQAv2/OCRVQA \
 
 ################################################################################################
 
 bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/OCRVQA/Finetune \
-
-################################################################################################
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ScienceQA/Zero_Shot \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/TextVQA/Zero_Shot \
-
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/ImageNet/Zero_Shot \
-
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/GQA/Zero_Shot \
-
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VizWiz/Zero_Shot \
-
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/Grounding/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/Grounding/Zero_Shot \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VQAv2/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/VQAv2/Zero_Shot \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/OCRVQA/Multitask \
-
-bash ./scripts/Eval_GeneralKnowledge/eval_prompt.sh \
-    ./results/CoIN/MiniGPTv2/OCRVQA/Zero_Shot \
+    ./results/CoIN/LLaVA-10Typev2/OCRVQA/Finetune \
